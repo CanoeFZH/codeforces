9403effb4b3517f9b6156bd59b99804d04fc9674
zhf <zhf@zhfdeMacBook-Pro.local>
2020-02-02 17:14:30 +0800

    dp

--- committed/1295F.cpp
+++ committed/1295F.cpp
@@ -1,59 +1,73 @@
 #include <cstdio>
-#include <iostream>
+#include <cstring>
 #include <algorithm>
+#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
-using ll = long long;
+const int N = 110;
+const int MOD = 998244353;
+typedef long long ll;
 
-const int maxn = 2e5 + 5;
-int n;
-int p[maxn];
-ll a[maxn], t[maxn * 4], mn[maxn * 4];
+ll l[N], r[N], s[N], dp[N][N];
 
-void push_down(int x) {
-	t[x << 1] += t[x];
-	t[x << 1 | 1] += t[x];
-	mn[x << 1] += t[x];
-	mn[x << 1 | 1] += t[x];
-	t[x] = 0;
+ll quick_mod(ll x, int y) {
+    ll acc = 1;
+    for( ; y; y >>= 1) {
+        if (y & 1) {
+            acc = acc * x % MOD;
+        }
+        x = x * x % MOD;
+    }
+    return acc;
 }
 
-void update(int l, int r, int x, int a, int b, int d) {
-	if(l > b || r < a) {
-		return;
-	}
-	if(l >= a && r <= b) {
-		t[x] += d;
-		mn[x] += d;
-		return;
-	}
-	push_down(x);
-	int mid = (l + r) >> 1;
-	update(l, mid, x << 1, a, b, d);
-	update(mid + 1, r, x << 1 | 1, a, b, d);
-	mn[x] = min(mn[x << 1], mn[x << 1 | 1]);
+ll cnm(ll n, int m) {
+    ll acc = 1;
+    for(int i = 1; i <= m; i++) {
+        acc = acc * (n + m - i) * quick_mod(i, MOD - 2) % MOD;
+    }
+    return acc;
 }
 
 int main() {
-	scanf("%d", &n);
-	for(int i = 1; i <= n; ++i) {
-		scanf("%d", &p[i]);
-	}
-	ll ans = 0;
-	for(int i = 1; i <= n; ++i) {
-		scanf("%lld", &a[i]);
-		ans += a[i];
-	}
-	for(int i = 1; i <= n; ++i) {
-		update(0, n, 1, p[i], n, a[i]);
+    int n;
+    ll total_size = 1;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> l[i] >> r[i];
+        s[i * 2 - 1] = l[i];
+        s[i * 2] = ++r[i];
+        total_size = (r[i] - l[i]) * total_size % MOD;
     }
 
-	for(int i = 1; i < n; ++i) {
-		update(0, n, 1, p[i], n, -a[i]);
-		update(0, n, 1, 0, p[i] - 1, a[i]);
-		ans = min(ans, mn[1]);
-	}
-	printf("%lld\n", ans);
-	return 0;
+    sort(s + 1, s + 2 * n + 1);
+    int points = unique(s + 1, s + 2 * n + 1) - (s + 1);
+    for (int i = 1; i <= n; i++) {
+        l[i] = lower_bound(s + 1, s + points + 1, l[i]) - s;
+        r[i] = lower_bound(s + 1, s + points + 1, r[i]) - s;
+    }
+
+    for (int i = 1; i <= points + 1; i++) {
+        dp[0][i] = 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for(int j = l[i]; j < r[i]; j++) {
+            for (int k = i - 1; k >= 0; k--) {
+                //cout << "i:" << i << " j:" << j << " k:" << k << " size:" << s[j + 1] - s[j] << endl;
+                dp[i][j] = (dp[i][j] + dp[k][j + 1] * cnm(s[j + 1] - s[j], i - k)) % MOD;
+                if (l[k] > j || r[k] <= j) {
+                    break;
+                }
+            }
+        }
+        for (int j = points; j >= 1; j--) {
+            dp[i][j] = (dp[i][j] + dp[i][j + 1]) % MOD;
+        }
+    }
+
+    cout << dp[n][1] * quick_mod(total_size, MOD - 2) % MOD << endl;
+    return 0;
 }
