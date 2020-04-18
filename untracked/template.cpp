682e1bb3435f57d71156d01ca654b435a7027940
canoe <you@example.com>
2020-04-18 17:22:24 +0800

    template

---
+++ untracked/template.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int N = 3e3;
+
+vector<vector<int> > v;
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("input", "r", stdin);
+#endif
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.time(0);
+  return 0;
+}
