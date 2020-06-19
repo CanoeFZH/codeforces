a42ba302a5959a7df20b2bf996e45f4e6dbff51a
zhf <zhf@microsoft.com>
2020-06-19 19:26:29 +0800

    interactive

--- untracked/template.cpp
+++ untracked/template.cpp
@@ -3,19 +3,23 @@
 #include <cstring>
 #include <iostream>
 #include <vector>
+#include <random>
+#include <chrono>
+#include <algorithm>
 
 using namespace std;
 
-const int N = 3e3;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-vector<vector<int> > v;
-
+//shuffle(d.begin(), d.end(), rng);
+//int i = uniform_int_distribution<int>(1, n)(rng);
+//
 int main() {
 #ifndef ONLINE_JUDGE
   freopen("input", "r", stdin);
 #endif
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cout.time(0);
+  cout.tie(0);
   return 0;
 }
